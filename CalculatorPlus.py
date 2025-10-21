455054f4e63a9b6b6764bf18d0853088d6b0b1d9
shivanisaurabh <saurabh.shivani3@gmail.com>
2025-10-21 12:29:36 +0530

    Continue sqrt feature work after merge with bug fix

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -31,5 +31,5 @@ print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
 print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
 
 # TODO: Uncomment and test the square root feature.
-# num3 = 25
-# print(f"The square root of {num3} = {calculator.square_root(num3)}")
+num3 = 25
+print(f"The square root of {num3} = {calculator.square_root(num3)}")
